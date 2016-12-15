a452f3738979339b52b68fe0b58851766567f606
run25 <t25.okano@gmail.com>
2016-12-16 00:55:29 +0900

    Hello

---
+++ HelloWorld/HelloWorld.Droid/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace HelloWorld.Droid
+{
+	[Activity (Label = "HelloWorld", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+            MobileCenter.Configure("5df6145c-b7f2-43f7-9874-d3038c057842");
+            base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new HelloWorld.App ());
+		}
+	}
+}
+
